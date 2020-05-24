8b6d40e221711570fa29385687f4cbb1eb14d2ac
Khamar-Uz-Zama <khamarzama007@gmail.com>
2020-05-24 12:16:59 +0530

    Update rnnFromScratch.py

--- rnnFromScratch.py
+++ rnnFromScratch.py
@@ -29,9 +29,10 @@ ind_to_ch = {ind: ch for (ch, ind) in vocab.items()}
 
 #print(vocab)
 #print(vocab_size)
+units = 256
+oneHotData = data.map(lambda char: tf.one_hot(indices = char, depth = vocab_size))
 
-oneHotData = data.map(lambda z: tf.one_hot(indices = z, depth = vocab_size))
-
+batch_OneHot_data = oneHotData.batch(batch_size=128, drop_remainder=True)
 
 def initializer(hunits, vocab_size, sequenceLength):
     trainVariables = {}
@@ -52,20 +53,20 @@ def initializer(hunits, vocab_size, sequenceLength):
     return trainVariables
 
 
-units = 256
 trainVariables = initializer(units, vocab_size, sequenceLength)
-batch_OneHot_data = oneHotData.batch(batch_size=128, drop_remainder=True)
+
 
 for key,value in trainVariables.items():
     print(key, '' , value.shape)
     
     
 n_time_steps = 100
+train_batch = next(iter(batch_OneHot_data))
 for time_step in range(n_time_steps):
-    train_batch = next(iter(batch_OneHot_data))
     
+    test_batch = next(iter(batch_OneHot_data))
     ## FeedForward computations
-    for instance in train_batch:
+    for instance,label in zip(train_batch,test_batch):
         # First node
         op_InpToHidden = tf.matmul(trainVariables['W_InpToHidden'],tf.transpose(instance)) + trainVariables['b_InpToHidden']
         op_HiddenToHidden = tf.matmul(trainVariables['W_HiddenToHidden'],trainVariables['t-1_HiddenToHidden'])
@@ -73,8 +74,8 @@ for time_step in range(n_time_steps):
         op_FromHidden = tf.nn.tanh(op_FromHidden)
         
         # Second node
-        op_FromOPLayer = tf.matmul(trainVariables['W_HiddenToOutput'],op_FromHidden) + trainVariables['b_HiddenToOutput']
+        op_FromOPLayer = tf.matmul(tf.transpose(trainVariables['W_HiddenToOutput']),op_FromHidden) + trainVariables['b_HiddenToOutput']
         y = tf.nn.softmax(logits = op_FromOPLayer)
-
-    
-    
+        
+        loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels = label, logits = op_FromOPLayer))
+    train_batch = test_batch
